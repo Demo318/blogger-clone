02cfdaabea4b5bdec1de2427613e827e2409df73
Devin Mork <devin.mork@gmail.com>
2017-05-05 22:51:27 -0500

    Try to fix typo.

--- db/migrate/20170506032144_create_taggings.rb
+++ db/migrate/20170506032144_create_taggings.rb
@@ -1,7 +1,7 @@
 class CreateTaggings < ActiveRecord::Migration[5.0]
   def change
     create_table :taggings do |t|
-      t.referenes :tag
+      t.references :tag
       t.references :article, foreign_key: true
 
       t.timestamps
