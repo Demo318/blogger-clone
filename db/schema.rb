c2d28dada80ab2ef4de4f3ab6391e1a00de37d14
Devin Mork <devin.mork@gmail.com>
2017-05-05 23:26:48 -0500

    Fix DB issue.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506024952) do
+ActiveRecord::Schema.define(version: 20170506032144) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20170506024952) do
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
-    t.string   "article"
-    t.string   "references"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
